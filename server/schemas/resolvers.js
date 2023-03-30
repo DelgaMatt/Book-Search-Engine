b8f51eb69ceb759f631fd720cb2cd448fca08e5c
DelgaMatt <115049801+DelgaMatt@users.noreply.github.com>
2023-03-29 18:34:04 -0700

    typeDefs defined

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,4 +2,12 @@ const { AuthenticationError } = require('apollo-server-express');
 const { User, Books } = require('../models');
 const signToken = require('../utils/auth');
 
+const resolvers = {
+    Query: {
+        user: async (parent, {email}) => {
+            return User.findOne({email: email}).populate('savedBooks');
+        }
+    }
+}
+
 module.exports = resolvers;
